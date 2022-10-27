86be2f12a551289051abd398be565e7a51e76c95
Tom <Tom@gitgit.com>
2022-10-27 11:14:58 +0800

    hot-fix commit1

--- src/main/java/com/ssm/controller/EmployeeController.java
+++ src/main/java/com/ssm/controller/EmployeeController.java
@@ -42,7 +42,7 @@ public class EmployeeController {
 
     @RequestMapping(value = "/employee",method = RequestMethod.GET)
     public String getAllEmployee(Model model){
-        System.out.println(123456);
+        System.out.println(123456+"hot-fix");
         //查询所有的员工信息-->/employee-->get
         List<Employee> list = employeeService.getAllEmployee();
         //将员工信息在请求域中共享
